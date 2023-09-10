d3873398ee4c7a074fa66895aba39759c444faba
sandratoo <toosandraa@gmail.com>
2023-09-10 13:45:46 +0300

    update gitignore

--- hngx.py
+++ hngx.py
@@ -7,13 +7,19 @@ app = Flask(__name__)
 def get_details():
     slack_name = request.args.get('slack_name')
     track = request.args.get('track')
-    day = 'Saturday'
+    day = 'Sunday'
     utc_time = datetime.datetime.now()
     file_URL = 'https://github.com/sandratoo/hngx/blob/main/hngx.py'
     source_code_URL = 'https://github.com/sandratoo/hngx.git'
     
 
-    return jsonify({'slack_name':slack_name,'track':track, 'day':day,"utc_time": utc_time,'status_code': 200, "github_repo_url": source_code_URL,"github_file_url": file_URL})
+    return jsonify({'slack_name':slack_name,
+    'track':track,
+     'day':day,
+     "utc_time": utc_time,
+     'status_code': 200,
+      "github_repo_url": source_code_URL,
+      "github_file_url": file_URL})
 
 if __name__ == ("__main__"):
     app.run(debug=True)
