8304050d30c68db906ec03a6cfb05dd21dc25062
sandratoo <toosandraa@gmail.com>
2023-09-11 09:32:04 +0300

    datetime format

--- app.py
+++ app.py
@@ -8,14 +8,14 @@ def get_details():
     slack_name = request.args.get('slack_name')
     track = request.args.get('track')
     day = 'Monday'
-    utc_time = datetime.datetime.now().isoformat("T", "milliseconds") + 'Z'
+    utc_time = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
     file_URL = 'https://github.com/sandratoo/hngx/blob/main/hngx.py'
     source_code_URL = 'https://github.com/sandratoo/hngx.git'
     
 
     return jsonify({'slack_name':slack_name,
     'track':track,
-    'day':day,
+    'current_day':day,
     'utc_time': utc_time,
     'status_code': 200,
     'github_repo_url': source_code_URL,
